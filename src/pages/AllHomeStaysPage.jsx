96203b737c8dee4bbf41c606cd70611e8e448cf8
pv-kiran <kiranpv903@gmail.com>
2024-11-24 14:42:43 +0530

    no results layout fixed

--- src/pages/AllHomeStaysPage.jsx
+++ src/pages/AllHomeStaysPage.jsx
@@ -144,7 +144,7 @@ function AllHomeStaysPage() {
                             Open Filters
                         </button>
 
-                        <div className={`${homeStayError ? "flex justify-center items-center" : "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"}`}>
+                        <div className={`${homeStayError ? "flex justify-center items-center h-[60vh] " : "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"}`}>
                             {
                                 homestaysLoading ? [...Array(12)].map((_, index) => (
                                     <HomestayCardSkeleton key={index} />
